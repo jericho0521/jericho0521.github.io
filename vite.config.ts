57fa083db1f7fdf14737a60b43cb1173a8fc4334
Jericho <73098876+jericho0521@users.noreply.github.com>
2025-12-14 02:19:46 +0800

    path change

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  base: '/neuroweb/',  // Your repo name
+  base: '/jericho0521.github.io/', 
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
