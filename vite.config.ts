c8816a5961025f63821947ed03288f0f6cec78d8
Jericho <73098876+jericho0521@users.noreply.github.com>
2025-12-14 14:00:26 +0800

    dir

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  base: '/jericho0521.github.io/', 
+  base: '/',
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
